f34e8350815c305643656d48e4c4aac18ab5a61d
Jorge Tejeda <jorgetejeda0804@gmail.com>
2024-08-09 18:48:58 -0400

    rollback

--- src/utils/cleanHtml.ts
+++ src/utils/cleanHtml.ts
@@ -1,10 +1,10 @@
 import renderHTML from "react-render-html";
 import DOMPurify from "dompurify";
 
-export const cleanHtml = (html: string): string => {
+export const cleanHtml = (html: string) => {
   const cleanHtml = DOMPurify.sanitize(html, {
     USE_PROFILES: { html: true },
   });
   const renderedHtml = renderHTML(cleanHtml);
-  return renderedHtml ? renderedHtml.toString() : '';
+  return renderedHtml ? renderedHtml : '';
 };
