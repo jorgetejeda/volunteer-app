1c3a27e666dfdb729868e7e7c415049e3864387a
Jorge Tejeda <jorgetejeda0804@gmail.com>
2024-08-11 17:40:45 -0400

    remove unneccesary export

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,5 @@
 export { lightOrDarkColor } from "./lightOrDarkColor";
 export { cleanHtml } from "./cleanHtml";
 export { compressImages } from "./compressImage";
-export { formatDate, formatTime, combineDateAndTime } from "./date";
+export { formatDate, combineDateAndTime } from "./date";
 export { elipsisText } from "./text";
