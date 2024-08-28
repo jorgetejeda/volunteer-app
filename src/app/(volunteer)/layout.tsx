43f937a7c98db651fab2495f30ddd355e057b7f8
Jorge Tejeda <jorgetejeda0804@gmail.com>
2024-08-27 21:45:32 -0400

    attempt axios request

--- src/app/(volunteer)/layout.tsx
+++ src/app/(volunteer)/layout.tsx
@@ -6,12 +6,12 @@ import { Header, Footer } from "@components/index";
 import { useSession } from "next-auth/react";
 
 export default function EventLayout(props: { children: React.ReactNode }) {
-  // const { data: session, status } = useSession();
+  const { data: session, status } = useSession();
 
-  // React.useLayoutEffect(() => {
-  //   if (session && session.token && !sessionStorage.getItem("token"))
-  //     sessionStorage.setItem("token", session.token);
-  // }, [session, status]);
+  React.useLayoutEffect(() => {
+    if (session && session.token && !sessionStorage.getItem("token"))
+      sessionStorage.setItem("token", session.token);
+  }, [session, status]);
 
   return (
     <Box
